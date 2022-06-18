b5dfd1c8eb960386e0c22e762d3ac29c5f0e54c3
Vladislav <vladislav2498@gmail.com>
2022-06-18 15:21:35 +0300

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,46 @@
-import gulp from 'gulp';
-import gulpSass from "gulp-sass";
-import nodeSass from "sass";
-const sass = gulpSass(nodeSass);
-import BS from 'browser-sync'
-const browserSync = BS.create();
-const { src, dest, task, watch, series, parallel } = gulp;
+import gulp from "gulp";
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
 
-const convertCss = () => src('src/styles/**/*')
+const sass = gulpSass(dartSass);
+// import htmlmin from "gulp-htmlmin";
+import concat from "gulp-concat";
+import terser from "gulp-terser";
+import cleancss from "gulp-clean-css";
+import imagemin from "gulp-imagemin";
+import browserSync from "browser-sync"
+
+const bS = browserSync.create();
+
+// export const htmlMin = () => gulp.src("./src/*.html")
+//     .pipe(htmlmin({collapseWhitespace: true}))
+//     .pipe(gulp.dest('./dist'))
+
+export const css = () => gulp.src("./src/**/*.scss")
     .pipe(sass())
-    .pipe(dest('dist/styles'));
+    .pipe(concat('styles.min.css'))
+    .pipe(cleancss())
+    .pipe(gulp.dest('./dist'))
 
-const startWatching = () => {
-    browserSync.init({
+export const js = () => gulp.src("./src/**/*.js")
+    .pipe(concat('script.min.js'))
+    .pipe(terser())
+    .pipe(gulp.dest('./dist'))
+
+export const minImage = () => gulp.src("./src/img/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/img'))
+
+export const build = gulp.parallel(css, js, minImage)
+
+export const dev = () => {
+    bS.init({
         server: {
-            baseDir: "./"
+            baseDir: '.'
         }
     });
-    watch('src/styles/**/*').on('all', series(convertCss, browserSync.reload));
+    gulp.watch('./src/**/*', gulp.series(build, (done) => {
+        bS.reload()
+        done();
+    }));
 }
-
-task('dev', startWatching);
-task('build', convertCss);
